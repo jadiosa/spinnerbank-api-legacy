a598d68826bcc7351e12260b5bf26f28d60b74b4
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2015-10-13 19:42:10 -0500

    Implements initial config with Hapijs

--- models/index.js
+++ models/index.js
@@ -1,23 +1,32 @@
-"use strict";
+'use strict';
 
-var fs        = require("fs");
-var path      = require("path");
-var Sequelize = require("sequelize");
-var sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://makers:12345678@localhost:5432/spinner_bank');
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
+    return (file.indexOf('.') !== 0) && (file !== basename);
   })
   .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
+    if (file.slice(-3) !== '.js') return;
+    var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
+  if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
