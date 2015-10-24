b2c4275641006c6bae12db215c3a5eed00af0250
Laura Martinez Londoño <laumarlon@yahoo.com.mx>
2015-10-23 21:47:16 -0500

    Add migration create Transaction

---
+++ migrations/20151023192008-create-transaction.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      value: {
+        type: Sequelize.DECIMAL(10, 2)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Transactions');
+  }
+};
