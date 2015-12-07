b371741092c841852640ef6db92e6f2c5b0363f4
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2015-12-07 00:38:35 -0500

    Fix the balance for each product from customers

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,13 @@
-var mongoose = require('mongoose');  
+var mongoose = require('mongoose');
+var async = require('async');
+
 var Product  = mongoose.model('Product');
 var Customer  = mongoose.model('Customer');
 var Transaction  = mongoose.model('Transaction');
 
 exports.findAllCustomerProducts = function(req, res) {
   Customer.findOne({email: req.params.email}, function(err, customer){
-    if(err) res.send(500, err.message);
+    if(err) res.status(500).send(err.message);
     
     if(customer === null){
       console.log('Customer not found');
@@ -13,28 +15,49 @@ exports.findAllCustomerProducts = function(req, res) {
     }
     else {
       Product.find({_id: customer.products}, function(err, products){
-        if(err) res.send(500, err.message);
+        if(err) res.status(500).send(err.message);
         
-        Transaction.findOne({}, {}, { sort: { 'createdAt' : -1 } }, function(err, transaction) {
-          if(err) res.send(500, err.message);
-          
-          var balance = 0;
-          if(transaction !== null){
-            balance = transaction.balance;
-          }
-          
-          var customerProductsJson = {
-            _id:        customer.id,
-            name:       customer.name,
-            lastName:   customer.lastName,
-            balance:    balance,
-            products:   products
-          }
-          
-          console.log('GET /customers/' + customer.email + '/products');
-          res.status(200).jsonp(customerProductsJson);
-        });
+        async.map(products, function(product, callback) {
+          setProductBalances(product, customer, callback);
+        },function(err, productsJson){
+            if( err ) {
+              res.status(500).send(err.message);
+            } else {
+              var customerProductsJson = {
+                _id:        customer.id,
+                name:       customer.name,
+                lastName:   customer.lastName,
+                products:   productsJson
+              };
+
+              console.log('GET api/v1/customers/' + customer.email + '/products');
+              res.status(200).jsonp(customerProductsJson);
+            }
+          });
       }); 
     }
   });
 };
+
+function setProductBalances(product, customer, callback) {
+  var productsJson = [];
+  Transaction.findOne({idProduct: product.id, idCustomer: customer.id}, {}, { sort: { 'createdAt' : -1 } }, function(err, transaction) {
+    if(err) return callback(err);
+    
+    var balance = 0;
+    if(transaction !== null){
+      balance = transaction.balance;
+    }
+    
+    var productJson = {
+      _id:          product.id,
+      name:         product.name,
+      productType:  product.productType,
+      balance:      balance
+    };
+    
+    productsJson.push(productJson);
+    
+    callback(null, productsJson);
+  });
+}
